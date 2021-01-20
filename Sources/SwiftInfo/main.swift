464f9b2c7aad2bfbd85bcf0039e1bde36d33e366
Cornel Chitanu <tap@dolfn.com>
2021-01-20 16:33:55 +0100

    Revert to 2.3.13

--- Sources/SwiftInfo/main.swift
+++ Sources/SwiftInfo/main.swift
@@ -1,24 +1,52 @@
+import ArgumentParser
 import Foundation
 import SwiftInfoCore
 
 let task = Process()
 
-public struct Main {
-    static func run() {
-        let fileUtils = FileUtils()
-        let toolchainPath = getToolchainPath()
-        log("SwiftInfo 2.3.12")
-        if ProcessInfo.processInfo.arguments.contains("-version") {
-            exit(0)
+struct Swiftinfo: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Swiftinfo 2.3.14",
+        subcommands: []
+    )
+
+    /// Since we are using .unconditionalRemaining, --help doesnt work anymore.
+    /// Thus, we added a manual flag here.
+    @Flag(name: .shortAndLong, help: "Show help information.")
+    var help = false
+
+    @Flag(name: .shortAndLong, help: "Silences all logs.")
+    var silent = false
+
+    @Flag(name: .shortAndLong, help: "Logs additional details to the console.")
+    var verbose = false
+
+    @Flag(name: .shortAndLong, help: "Logs SourceKit requests to the console.")
+    var printSourcekit = false
+
+    @Argument(parsing: .unconditionalRemaining, help: "Any additional arguments that you would like your Infofile.swift to receive. These arguments can be retrieved in your Infofile through CommandLine's APIs to customize your runs.")
+    var arguments: [String] = []
+
+    mutating func run() throws {
+        if help {
+            print(Swiftinfo.helpMessage())
+            Swiftinfo.exit()
         }
+
+        setupLogConfig()
+        guard let executablePath = CommandLine.arguments.first else {
+            fail("Couldn't determine the folder that's running SwiftInfo.")
+        }
+        let fileUtils = FileUtils(path: executablePath)
+        let toolchainPath = Swiftinfo.getToolchainPath()
+
         log("Dylib Folder: \(fileUtils.toolFolder)", verbose: true)
         log("Infofile Path: \(try! fileUtils.infofileFolder())", verbose: true)
         log("Toolchain Path: \(toolchainPath)", verbose: true)
 
-        let processInfoArgs = ProcessInfo.processInfo.arguments
         let args = Runner.getCoreSwiftCArguments(fileUtils: fileUtils,
                                                  toolchainPath: toolchainPath,
-                                                 processInfoArgs: processInfoArgs)
+                                                 processInfoArgs: Array(CommandLine.arguments.dropFirst()))
             .joined(separator: " ")
 
         log("Swiftc Args: \(args)", verbose: true)
@@ -29,7 +57,7 @@ public struct Main {
         task.standardError = FileHandle.standardError
 
         task.terminationHandler = { t -> Void in
-            exit(t.terminationStatus)
+            Swiftinfo.exit()
         }
 
         task.launch()
@@ -50,6 +78,12 @@ public struct Main {
         let oneLined = developer.replacingOccurrences(of: "\n", with: "")
         return oneLined + "/Toolchains/XcodeDefault.xctoolchain/usr/lib/sourcekitd.framework/sourcekitd"
     }
+
+    private func setupLogConfig() {
+        isInVerboseMode = verbose
+        isInSilentMode = silent
+        printSourceKitQueries = printSourcekit
+    }
 }
 
 /////////
@@ -64,5 +98,5 @@ source.setEventHandler {
 ////////
 
 source.resume()
-Main.run()
+Swiftinfo.main()
 dispatchMain()
